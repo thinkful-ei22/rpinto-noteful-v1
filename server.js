d87b4ead987308b7aca6dc391f70902a17cb212e
rpinto <rachel.lea.pinto@gmail.com>
2018-06-26 11:59:37 -0700

    create config.js module

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 'use strict';
 
-// Load array of notes
+const { PORT } = require('./config')
+
+// Load array of notes // local library
 const data = require('./db/notes');
 
 console.log('Hello Noteful!');
 
-// INSERT EXPRESS APP CODE HERE...
+// INSERT EXPRESS APP CODE HERE... // external library
 
 const express = require('express');
 
@@ -14,27 +16,21 @@ const app = express();
 // ADD STATIC SERVER HERE
 app.use(express.static('public'));
 
-//Search filter logic
+//Search filter logic 
 app.get('/api/notes', (req, res) => {
   const searchTerm = req.query.searchTerm;
   if (!searchTerm) return res.json(data);
   const filteredData = data.filter(function(item) {
-    if (item.title.includes(searchTerm)) {
-      return true;
-    } 
-    else {
-      return false;
-    }
+    return (item.title.includes(searchTerm)); 
   })
   res.json(filteredData);
 });
 
 app.get('/api/notes/:id', (req, res) => {
-  res.json(data.find(item => item.id === Number(id)));
+  res.json(data.find(item => item.id === Number(req.params.id)));
 });
 
-
-app.listen(8080, function () {
+app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);

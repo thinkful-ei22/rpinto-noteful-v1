06d900fcf44fc699be30d92151605710c64b67a4
rpinto <rachel.lea.pinto@gmail.com>
2018-06-27 14:24:26 -0700

    createsd POST endpoint

--- router/notes.router.js
+++ router/notes.router.js
@@ -45,6 +45,31 @@ router.get('/notes/:id', (req, res) => {
   });
 });
 
+// Post (insert) an item
+router.post('/notes', (req, res, next) => {
+ 
+  const { title, content } = req.body;
+  const newItem = { title, content };
+  /***** Never trust users - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+//PUT
 router.put('/notes/:id', (req, res, next) => {
   const id = req.params.id;
 
